855525a15499b71206d79809ac07735b646ef131
Laxmi Chandra Dhuvare <lcdhuvare30@gmail.com>
2021-03-22 00:16:19 +0530

    header menu done

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 
 import { MatSliderModule } from '@angular/material/slider';
 import { FooterComponent } from './footer/footer.component';
@@ -15,14 +16,23 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { ElishCustomMaterialModule } from './common/custom.material';
+import { HelpdeskComponent } from './helpdesk/helpdesk.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    HelpdeskComponent,
+    LoginComponent,
+    SignupComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +9,24 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  @Input() imageUrl: string='dashboard';
+  @Input() pageTitle: string;
+  @Input() helpType: string;
+  emailSent = false;
+  selectedValue: any;
+  formShowing = false;
+  configData: any={
+    fblink:'',
+    linkedin:'',
+    github:'',
+    emailid:'',
+  };
+  userRole = 'student';
+  msgCount$: Observable<any>;
+
+  error: any;
+  dataLoading: boolean = false;
+  brokenNetwork = false;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
